ec75141133248686d60b3d3ec588850ba5ef9498
Akay74 <akaudeh@gmail.com>
2024-09-20 12:55:16 +0100

    🎨 remove header & nav code

--- components/home/hero.jsx
+++ components/home/hero.jsx
@@ -1,108 +1,71 @@
+"use client"
 import React from 'react';
+import Image from 'next/image';
+import heroImg from '../../assets/hero.jpg';
 import styled from 'styled-components';
-import { FaFacebookF, FaInstagram, FaTwitter, FaAmazon, FaApple, FaSoundcloud, FaSpotify, FaYoutube } from 'react-icons/fa';
-import Link from 'next/link';
 
 const HeroContainer = styled.div`
-  background-color: #000;
-  color: #fff;
-  height: 100vh;
-  display: flex;
-  position: relative;
-`;
-
-const SidebarIcons = styled.div`
-  position: fixed;
-  left: 20px;
-  top: 50%;
-  transform: translateY(-50%);
+  background-color: #fff;
+  color: #000;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
-  gap: 20px;
-`;
-
-const IconLink = styled.a`
-  color: #fff;
-  font-size: 24px;
-  transition: color 0.3s ease;
-
-  &:hover {
-    color: #ffff00;
+  @media (min-width: 768px) {
+    flex-direction: row;
   }
 `;
 
 const Content = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 40px;
-`;
-
-const Logo = styled.h1`
-  font-size: 48px;
-  font-weight: bold;
-`;
-
-const Navigation = styled.nav`
-  display: flex;
-  gap: 20px;
-`;
-
-const NavLink = styled.a`
-  color: #ffff00;
-  text-decoration: none;
-  font-weight: bold;
-  text-transform: uppercase;
+  padding: 20px;
+  @media (min-width: 768px) {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    padding: 40px;
+  }
 `;
 
 const AlbumInfo = styled.div`
-  margin-bottom: 40px;
+  width: 50%;
+  text-align: center;
+  margin: auto;
 `;
 
 const AlbumTitle = styled.h2`
   font-size: 36px;
+  font-weight: 700;
+  letter-spacing: 15px;
   color: #a0522d;
 `;
 
 const ReleaseInfo = styled.p`
-  font-size: 24px;
+  font-size: 30px;
+  font-weight: 700;
   color: #a0522d;
 `;
 
-const HeroImage = styled.img`
-  width: 50%;
+const HeroImage = styled(Image)`
+  width: 100%;
+  height: auto;
   object-fit: cover;
-  object-position: center;
+  object-position: center top;
+  @media (min-width: 768px) {
+    width: 50%;
+    height: 100vh;
+    object-position: center;
+  }
 `;
 
 const Hero = () => {
   return (
     <HeroContainer>
-      <SidebarIcons>
-        <IconLink href="#"><FaFacebookF /></IconLink>
-        <IconLink href="#"><FaInstagram /></IconLink>
-        <IconLink href="#"><FaTwitter /></IconLink>
-        <IconLink href="#"><FaAmazon /></IconLink>
-        <IconLink href="#"><FaApple /></IconLink>
-        <IconLink href="#"><FaSoundcloud /></IconLink>
-        <IconLink href="#"><FaSpotify /></IconLink>
-        <IconLink href="#"><FaYoutube /></IconLink>
-      </SidebarIcons>
       <Content>
-        <Logo>2CHAINZ</Logo>
-        <Navigation>
-          <NavLink href="#">MUSIC</NavLink>
-          <NavLink href="#">VIDEOS</NavLink>
-          <NavLink href="#">TOUR</NavLink>
-          <NavLink href="#">LOGIN</NavLink>
-        </Navigation>
         <AlbumInfo>
-          <AlbumTitle>WELCOME 2 COLLEGROVE</AlbumTitle>
+          <AlbumTitle>THESE RICO TIMES</AlbumTitle>
           <ReleaseInfo>OUT NOW</ReleaseInfo>
         </AlbumInfo>
+        <HeroImage src={heroImg} alt="roi hero image" />
       </Content>
-      <HeroImage src="/path-to-hero-image.jpg" alt="2Chainz Album Cover" />
     </HeroContainer>
   );
 };
