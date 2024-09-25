2ac8e6252b26d5521c88038d14512fd33b6f30ea
Akay74 <akaudeh@gmail.com>
2024-09-25 11:52:33 +0100

    🎨 import homepage component

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
-import Hero from "@/components/home/hero";
 import HeaderComponent from "@/components/home/header";
-import MusicSection from "@/components/home/music";
+import Homepage from "@/components/home/homepage";
 
 
 export default function Home() {
   return (
     <main>
       <HeaderComponent />
-      <Hero />
-      <MusicSection />
+      <Homepage />
     </main>
   );
 }
