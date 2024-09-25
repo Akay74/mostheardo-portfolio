434cd7064acc3ade1a06eccbbb68a49e2da7ece3
Akay74 <akaudeh@gmail.com>
2024-09-25 11:50:21 +0100

    🎨 import homepage components

---
+++ components/home/homepage.jsx
@@ -0,0 +1,14 @@
+import Hero from "./hero";
+import MusicSection from "./music";
+import VideoSection from "./videos";
+
+
+export default function Homepage() {
+  return (
+    <main>
+      <Hero />
+      <MusicSection />
+      <VideoSection />
+    </main>
+  );
+}
