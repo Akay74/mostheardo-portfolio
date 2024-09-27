8aa3ae54885307cd4ed77707f73fffa1bd188f5a
Akay74 <akaudeh@gmail.com>
2024-09-28 00:56:53 +0100

    🎨 create a page.js for routing

---
+++ app/music/page.js
@@ -0,0 +1,14 @@
+import HeaderComponent from "@/components/home/header";
+import MusicPage from "./musicpage";
+import Footer from "@/components/home/footer";
+
+
+export default function Home() {
+  return (
+    <main>
+      <HeaderComponent />
+      <MusicPage />
+      <Footer />
+    </main>
+  );
+}
