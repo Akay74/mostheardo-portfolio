b8234540a41d0e89d4f398b23efa89a84c574c7c
Akay74 <akaudeh@gmail.com>
2024-09-27 09:20:35 +0100

    🎨 import Shop component into homepage

--- components/home/homepage.jsx
+++ components/home/homepage.jsx
@@ -1,6 +1,7 @@
 import Hero from "./hero";
 import MusicSection from "./music";
 import VideoSection from "./videos";
+import Shop from "./shop";
 
 
 export default function Homepage() {
@@ -9,6 +10,7 @@ export default function Homepage() {
       <Hero />
       <MusicSection />
       <VideoSection />
+      <Shop />
     </main>
   );
 }
