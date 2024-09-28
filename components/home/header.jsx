09241e95260c3ddf25ad365c9a9dcee694490874
Akay74 <akaudeh@gmail.com>
2024-09-28 02:40:39 +0100

    💄 update UI

--- components/home/header.jsx
+++ components/home/header.jsx
@@ -45,14 +45,14 @@ const HeaderComponent = () => {
   return (
     <>
       <div className="bg-gray-100 p-2.5 text-black fixed top-0 left-0 right-0 z-50">
-        <header className="flex h-[10vh] justify-between items-center p-5 md:p-8">
-          <h1 className="text-black text-2xl font-bold">MOSTHEARDOF</h1>
+        <header className="flex h-[5vh] justify-between items-center py-5 px-4 md:p-8">
+          <a href="/#" className="text-black text-xl md:text-2xl font-bold tracking-[2px] md:tracking-[6px] cursor-pointer">MOSTHEARDOF</a>
           <button onClick={toggleMenu} className="bg-transparent border-none text-black text-2xl cursor-pointer md:hidden">☰</button>
           <nav className="hidden md:flex md:justify-end md:items-center">
             <NavLinks isMobile={false} />
           </nav>
         </header>
-        <nav className={`fixed top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-[1001] md:hidden`}>
+        <nav className={`fixed top-0 left-0 w-[full] h-screen bg-black flex flex-col justify-center items-center transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-[1001] md:hidden`}>
           <NavLinks isMobile={true} onClick={toggleMenu} />
           <SocialIconsComponent isMobile={true} />
         </nav>
