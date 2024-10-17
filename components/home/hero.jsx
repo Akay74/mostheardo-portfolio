e394a6c1e74fa69182fa316752f2ececc867d40c
Akay74 <akaudeh@gmail.com>
2024-10-17 13:02:38 +0100

    🎨 adjust dimensions

--- components/home/hero.jsx
+++ components/home/hero.jsx
@@ -5,17 +5,17 @@ import heroImg from '../../assets/hero.jpg';
 
 const Hero = () => {
   return (
-    <div className="bg-white text-black min-h-screen flex flex-col md:flex-row">
+    <div className="bg-white text-black h-auto md:min-h-screen flex flex-col md:flex-row">
       <div className="p-5 md:p-10 md:flex md:w-full md:justify-between">
-        <div className="w-full md:w-1/2 text-center m-auto py-8">
-          <h2 className="text-3xl md:text-4xl font-bold tracking-[15px] text-[#a0522d]">
+        <div className="w-full md:w-1/2 text-center m-auto py-12 md:py-8">
+          <h2 className="text-3xl md:text-4xl font-[700] tracking-[13px] text-[#a0522d]">
             THESE RICO TIMES
           </h2>
           <p className="text-2xl md:text-3xl font-bold text-[#a0522d] py-4">
             OUT NOW
           </p>
         </div>
-        <div className="w-full md:w-1/2 h-auto md:h-screen">
+        <div className="w-full md:w-1/2 h-auto md:h-screen py-10 md:py-0">
           <Image 
             src={heroImg} 
             alt="roi hero image" 
