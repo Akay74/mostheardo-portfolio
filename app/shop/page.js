031e3588035f005bdaedd233c2624f598bea1451
Akay74 <akaudeh@gmail.com>
2024-10-20 18:11:01 +0100

    🎨 create a page file for the shop component

---
+++ app/shop/page.js
@@ -0,0 +1,14 @@
+import HeaderComponent from "@/components/home/header";
+import Shop from "./shop";
+import Footer from "@/components/home/footer";
+
+
+export default function Home() {
+  return (
+    <main>
+      <HeaderComponent />
+      <Shop />
+      <Footer />
+    </main>
+  );
+}
