175819713c424f79bbf3a866514fc5c24ba8b09a
Akay74 <akaudeh@gmail.com>
2024-10-20 18:48:43 +0100

    🎨 change text to link element, add link

--- components/home/hero.jsx
+++ components/home/hero.jsx
@@ -7,14 +7,18 @@ const Hero = () => {
   return (
     <div className="bg-white text-black h-auto md:min-h-screen flex flex-col md:flex-row">
       <div className="p-5 md:p-10 md:flex md:w-full md:justify-between">
-        <div className="w-full md:w-1/2 text-center m-auto py-12 md:py-8">
+        <a 
+          href="https://ditto.fm/these-rico-times"
+          target="_blank"
+          className="block w-full md:w-1/2 text-center m-auto py-12 md:py-8 no-underline hover:opacity-80 transition-opacity duration-300"
+        >
           <h2 className="text-3xl md:text-4xl font-[700] tracking-[13px] text-[#a0522d]">
             THESE RICO TIMES
           </h2>
           <p className="text-2xl md:text-3xl font-bold text-[#a0522d] py-4">
             OUT NOW
           </p>
-        </div>
+        </a>
         <div className="w-full md:w-1/2 h-auto md:h-screen py-10 md:py-0">
           <Image 
             src={heroImg} 
